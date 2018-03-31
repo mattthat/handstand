7d62946525971f2b38769b279b49198afcaef4ef
Matt That <matt.that@gmail.com>
2018-03-31 08:10:32 -0400

    HANDSTAND-75 3.0.0 HandstandTextarea (with autofocus support)

--- src/js/ui-elements/HandstandTextarea.js
+++ src/js/ui-elements/HandstandTextarea.js
@@ -1,31 +1,38 @@
-export class HandstandTextarea extends HandstandConfigurableElement {
-    get template() {
-        return `<textarea></textarea>`;
-    }
+import css from '../../css/ui-elements/HandstandTextarea.css';
+import HandstandCustomElement from '../ui-core/HandstandCustomElement.js';
+
+export default class HandstandTextarea extends HandstandCustomElement {
     get value() {
-        return this.model.Get('value') || '';
+        return this.conditions.properties.value;
     }
-    set value(text) {
-        if (this.textarea) this.textarea.value = text;
-        this.model.Set('value', text);
+    set value(v) {
+        this.conditions.properties.value = v;
+        this.textarea.value = v;
     }
-    constructor(attributes, options) {
-        super(attributes);
-        if (options) {
-            this.model.Set('value', options.value);
-        }
-        this.textarea = {};
+    constructor(conditions) {
+        super(conditions);
+        this.conditions.autofocus = conditions.autofocus;
+        this.textarea = document.createElement('textarea');
+        this.textarea.value = this.conditions.properties.value || '';
+        this.on('change', this.onChange.bind(this));
+        this.append(this.textarea);
+    }
+    onChange() {
+        if (this.textarea) this.value = this.textarea.value;
     }
     onRender() {
-        this.textarea = this.childNodes[0];
-        this.textarea.value = this.model.Get('value');
-    }
-    onSetHandler(key, value, model) {
-        if (this.textarea) this.textarea.value = model.Get('value');
-    }
-    onChange(e) {
-        this.model.Set('value', this.textarea.value);
+        if (this.conditions.autofocus)
+            this.textarea.setAttribute('autofocus', '');
+        if (this.conditions.properties.placeHolder)
+            this.textarea.placeholder = this.conditions.properties.placeHolder;
+        if (typeof this.conditions.events.onKeyUp === 'function')
+            this.textarea.onkeyup = this.conditions.events.onKeyUp.bind(this);
+        if (typeof this.conditions.events.onKeyDown === 'function')
+            this.textarea.onkeydown = this.conditions.events.onKeyDown.bind(this);
+        if (typeof this.conditions.events.onKeyPress === 'function')
+            this.textarea.onkeypress = this.conditions.events.onKeyPress.bind(this);
+        if (typeof this.conditions.events.onChange === 'function')
+            this.textarea.onchange = this.conditions.events.onChange.bind(this);
     }
 }
-HandstandConfigurableElement.tag('handstand-textarea', HandstandTextarea);
-module.exports = HandstandTextarea;
+customElements.define('handstand-textarea', HandstandTextarea);
